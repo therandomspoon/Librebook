667b8a46f59df452618330418d5a06e2fb2969d4
“therandomspoon” <admin@randspoon.co.uk>
2024-01-14 21:17:46 +0000

    more image formats allowed in a better way

--- messages/messages.php
+++ messages/messages.php
@@ -2,6 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 include '../config.php'; 
+
 try {
     $conn = $pdo->query("SELECT `name`, `message`, `timestamp`
                         FROM messages
@@ -16,7 +17,11 @@ try {
                 $message = htmlspecialchars($row["message"], ENT_QUOTES, 'UTF-8');
                 $timestamp = $row["timestamp"];
 
-                if (filter_var($message, FILTER_VALIDATE_URL) && (strpos($message, '.jpg') !== false || strpos($message, '.jpeg') !== false || strpos($message, '.png') !== false)) {
+                if (filter_var($message, FILTER_VALIDATE_URL) && 
+                    (strpos($message, '.jpg') !== false || 
+                     strpos($message, '.jpeg') !== false || 
+                     strpos($message, '.png') !== false || 
+                     strpos($message, '.webp') !== false)) {
                     echo "<div><b>" . $name . ":</b> <br> <img src='" . $message . "' alt='Image' style='width: 211px; height: 148px;'> <br> (Sent on: " . $timestamp . ")</div>";
                 } else {
                     echo "<div><b>" . $name . ":</b> " . $message . " (Sent on: " . $timestamp . ")</div>";
