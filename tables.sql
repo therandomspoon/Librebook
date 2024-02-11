d16b802cfe75c26085970ac7a41c24982bd00144
therandomspoon <admin@randspoon.co.uk>
2024-02-11 11:54:11 +0000

    urgent patches

--- tables.sql
+++ tables.sql
@@ -1,10 +1,11 @@
 -- run this code to create the user table;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `preferred_mode` varchar(10) DEFAULT 'light'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 
 -- run this one to create the messages table;
 CREATE TABLE `messages` (
@@ -13,3 +14,11 @@ CREATE TABLE `messages` (
   `message` text NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- IF it doesnt seem to work on the register code run these
+
+-- Modify the 'id' column to be an auto-incremented primary key
+ALTER TABLE `users` MODIFY `id` INT AUTO_INCREMENT PRIMARY KEY;
+
+-- Modify the 'id' column to be an auto-incremented primary key
+ALTER TABLE `messages` MODIFY `id` INT AUTO_INCREMENT PRIMARY KEY;
