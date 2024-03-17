7acddbfc1a28eba0665d6661c2e79f5e8fe97276
therandomspoon <admin@randspoon.co.uk>
2024-03-17 10:20:03 +0000

    Update 4

--- profiles/follow.php
+++ profiles/follow.php
@@ -21,7 +21,6 @@ if (empty($loginuser)) {
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $searchTerm = isset($_GET['search']) ? $_GET['search'] : '';
     
-    // Assuming the column representing the following is named 'following'
     $stmt = $pdo->prepare("UPDATE users SET following = 
                             CASE 
                                 WHEN following LIKE CONCAT('%', ?, '%') THEN 
