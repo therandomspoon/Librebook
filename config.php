46cab5840cd3ec6b3421f59aa0956a23c97fe124
therandomspoon <admin@randspoon.co.uk>
2024-10-05 21:44:04 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$db_host = 'localhost';
-$db_name = 'messages';
-$db_user = 'adamg';
-$db_password = 'pirules';
+$db_host = '';
+$db_name = '';
+$db_user = '';
+$db_password = '';
 $sacredwork = ''; // For a future unreleased update regarding verifying accounts 
 
 try {
