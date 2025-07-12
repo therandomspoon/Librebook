c75ed900471fcbbe706791b60265683b35271ba6
therandomspoon <admin@randspoon.co.uk>
2025-07-12 23:05:26 +0100

    Tecopa refinements

--- config.php
+++ config.php
@@ -2,11 +2,13 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$db_host = '';
-$db_name = '';
-$db_user = '';
-$db_password = '';
+$db_host = 'sql207.infinityfree.com';
+$db_name = 'if0_36414508_librebooktecopa';
+$db_user = 'if0_36414508';
+$db_password = 'ZaWPEfgCWEF';
 $sacredwork = ''; // For a future unreleased update regarding verifying accounts 
+
+$offsetnum = 10; // the number of messages loaded per page
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
