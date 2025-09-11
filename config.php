8867b6ed39c24c51fb853182975f984824b04189
therandomspoon <admin@randspoon.co.uk>
2025-09-11 07:38:35 +0100

    Atwood!

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$db_host = 'sql207.infinityfree.com';
-$db_name = 'if0_36414508_librebooktecopa';
-$db_user = 'if0_36414508';
-$db_password = 'ZaWPEfgCWEF';
+$db_host = '';
+$db_name = '';
+$db_user = '';
+$db_password = '';
 $sacredwork = ''; // For a future unreleased update regarding verifying accounts 
 
 $offsetnum = 10; // the number of messages loaded per page
